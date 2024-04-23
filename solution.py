bcdbe2686c02fe86ed05a6e91f5c97e14dca1595
lampros2003 <46668710+lampros2003@users.noreply.github.com>
2024-04-23 21:04:30 +0300

    Done

--- solution.py
+++ solution.py
@@ -1,7 +1,10 @@
 from ast import If
+from decimal import DivisionUndefined
 import time
 import pprint
 import random
+import math
+import numpy
 test_dataset = [[1,4] ,[1,14,-23,15] ,[-3,14,22,44,-14,32,-1,-98] 
 ,[12,41,-32,11,31,23,-22,-14,44,14,53,-31,15,-80,-14,100],[12,41,-32,11,31,23,-22,-14,44,14,53,-31,15,-80,-14,100,12,41,-32,11,31,23,-22,-14,44,14,53,-31,15,-80,-14,100] 
 ,[12,41,-32,11,31,23,-22,-14,44,14,53,-31,15,-80,-14,100,12,41,-32,11,31,23,-22,-14,44,14,53,-31,15,-80,-14,100,12,41,-32,11,31,23,-22,-14,44,14,53,-31,15,-80,-14,100,12,41,-32,11,31,23,-22,-14,44,14,53,-31,15,-80,-14,100]
@@ -9,7 +12,9 @@ test_dataset = [[1,4] ,[1,14,-23,15] ,[-3,14,22,44,-14,32,-1,-98]
 ,[random.randint(-100,100) for x in range(256)],[random.randint(-100,100) for x in range(512)]
 ,[random.randint(-100,100) for x in range(1024)]
 ]
-
+brute_test = [[random.randint(-100,100) for x in range(2**i)] for i in range(10,13) ]
+kadane_test= [[random.randint(-100,100) for x in range(((40157040)//2)*i)]for i in range(1,3)]
+divideandconquer_test = [[random.randint(-100,100) for x in range(((1607437)//2)*i)]for i in range(1,3)]
 
 def max_sum_divide_and_conquer(arr,length):
     indexlow =-199
@@ -56,9 +61,6 @@ def max_sum_divide_and_conquer(arr,length):
         return maxim
     
     return maxSubArraySum(arr, 0, length-1), indexlow,indexhigh
-max_sum_divide_and_conquer(test_dataset[1],len(test_dataset[1])) 
-
-
 def Bad_Brute_Force(arr,length):
     Maxsum = -1000
     indexlow =-199
@@ -92,7 +94,6 @@ def Kadane_algo(arr,length):
             max_at_index = 0
             temp = i+1
     return maxsum, indexlow , indexhigh
-
 def bruteforce(arr,length):
     Maxsum = -1000
     indexlow =0
@@ -107,8 +108,7 @@ def bruteforce(arr,length):
             Maxsum = max(sum,Maxsum)
             if Maxsum == sum:
                 indexhigh = end
-    return Maxsum,indexlow,indexhighS
-
+    return Maxsum,indexlow,indexhigh
 def counter(dataset):
     result = []
     for i in dataset:
@@ -130,6 +130,96 @@ def counter(dataset):
         t1_stop = time.perf_counter()
         t4 = t1_stop -t1_start 
         result.append({"Badbrute":t1,"Brute":t2,"Divide and Conquer":t3,"Kadane":t4})
+    with open('output.txt', 'wt') as out:
+        pprint.pprint(result, stream=out)
     return result
+#counter can find the increase of
+#Finding max array length that can be clalculated by kadane algo
+#Through observation we find that for n = 1000000 t = 0.0746sec
+#So having shown O(n) complexity then we find nmax = x t = 3
+#Where x = 40214477 elements for about 3 sec time
+#randint function would take too much time for these many elements so i didnt show it directly
+def maxkadane(n):
+    kadanetest=[random.randint(-100,100) for x in range(n)]
+    t1_start = time.perf_counter() 
+    Kadane_algo(kadanetest,n)
+    t1_stop = time.perf_counter()
+    tpartial_kadane = t1_stop -t1_start 
+    n_max_kadane = 3*n/(tpartial_kadane)
 
-pprint.pprint(counter(test_dataset))
+    with open('output.txt', "a") as out:
+        pprint.pprint("\nKadane  time for %i : %f "%(n,tpartial_kadane), stream=out)
+        pprint.pprint("\nKadane  est. max number elements for 3sec runtime : :%i "%n_max_kadane, stream=out)
+#Finding max array length that can be calculated by divide and conquer algo
+#Max num of elements 
+def maxdivide(n):
+    dividetest=[random.randint(-100,100) for x in range(n)]
+    t1_start = time.perf_counter() 
+    max_sum_divide_and_conquer(dividetest,n)
+    t1_stop = time.perf_counter()
+    tmax_divide = t1_stop -t1_start 
+    n_max_divide = 3*(n)/(tmax_divide)
+    with open('output.txt', "a") as out:
+        pprint.pprint("\nDivide and conquer time for %i elements :%f "%(n,tmax_divide), stream=out)
+        pprint.pprint("\nDivide and conquer est. max number elements for 3sec runtime :%i "%n_max_divide, stream=out)
+#Finding max array length that can be calculated by Brute algo
+def maxBrute(n):
+    dividetest=[random.randint(-100,100) for x in range(n)]
+    t1_start = time.perf_counter() 
+    bruteforce(dividetest,n)
+    t1_stop = time.perf_counter()
+    tmax_brute = t1_stop -t1_start 
+    const = tmax_brute/(n**2)
+    n_max_brute = math.sqrt(3/const)
+    with open('output.txt', "a") as out:
+        pprint.pprint("\nBrute force time for %i elements :  %f"%(n,tmax_brute), stream=out)
+        pprint.pprint("\nBrute force  est. max number elements for 3sec runtime : %i" %n_max_brute, stream=out)
+#Finding max array length that can be calculated by Nested Brute algo
+def maxBad_Brute_Force(n):
+    dividetest=[random.randint(-100,100) for x in range(n)]
+    t1_start = time.perf_counter() 
+    Bad_Brute_Force(dividetest,n)
+    t1_stop = time.perf_counter()
+    tmax_Badbrute = t1_stop -t1_start 
+    const = tmax_Badbrute/(n**3)
+    n_max_Badbrute = (3/const)**(1/3)
+    with open('output.txt', "a") as out:
+        pprint.pprint("\nBad_Brute_Force time for %i elements : %f"% (n, tmax_Badbrute), stream=out)
+        pprint.pprint("\nBad_Brute_Force  est. max number elements for 3sec runtime :  %i" %n_max_Badbrute, stream=out)
+
+def calculatemaxes():
+    maxBad_Brute_Force(730)
+    maxBrute(6000)
+    maxdivide(1600000)
+    maxkadane(1000000)
+def calc__for_each_set(test_dataset,brute_test,kadane_test,divideandconquer_test):    
+    with open('output.txt', "a") as out:
+        for i in test_dataset:
+            length = len(i)
+            t1_start = time.perf_counter() 
+            Bad_Brute_Force(i,length)
+            t1_stop = time.perf_counter()
+            t1 = t1_stop -t1_start 
+            pprint.pprint("test_dataset time for %i elements : %f"% (length, t1), stream=out)
+        for i in brute_test:
+            length = len(i)
+            t1_start = time.perf_counter() 
+            bruteforce(i,length)
+            t1_stop = time.perf_counter()
+            t1 = t1_stop -t1_start 
+            pprint.pprint("brute_test time for %i elements : %f"% (length, t1), stream=out)
+        for i in kadane_test:
+            length = len(i)
+            t1_start = time.perf_counter() 
+            Kadane_algo(i,length)
+            t1_stop = time.perf_counter()
+            t1 = t1_stop -t1_start 
+            pprint.pprint("kadane_test time for %i elements : %f"% (length, t1), stream=out)
+        for i in divideandconquer_test:
+            length = len(i)
+            t1_start = time.perf_counter() 
+            max_sum_divide_and_conquer(i,length)
+            t1_stop = time.perf_counter()
+            t1 = t1_stop -t1_start 
+            pprint.pprint("divideandconquer_test time for %i elements : %f"% (length, t1), stream=out)
+calc__for_each_set(test_dataset,brute_test,kadane_test,divideandconquer_test)
